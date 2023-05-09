7233c7926f45ecd21ca8f79036635b83bda72fdf
Jacob Miller <millerjr1181@gmail.com>
2023-05-09 10:57:25 -0400

    with compiled code

--- src/Calculations.java
+++ src/Calculations.java
@@ -1,2 +1,16 @@
-package PACKAGE_NAME;public class Calculations {
+import java.util.Scanner;
+public class Calculations {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Please enter your first integer: ");
+        int a = scanner.nextInt();
+        System.out.println("Please enter your second integer: ");
+        int b = scanner.nextInt();
+        System.out.println(a + " + " + b + " = " + (a+b));
+        System.out.println(a + " - " + b + " = " + (a-b));
+        System.out.println(a + " * " + b + " = " + (a*b));
+        System.out.println(a + " / " + b + " = " + (Double.valueOf(a)/Double.valueOf(b)));
+        System.out.println(a + " % " + b + " = " + (a%b));
+
+    }
 }
